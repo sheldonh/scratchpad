97ec341c26f2fc48189c4d764839b4674752d9a0
Sheldon Hearn <sheldonh@starjuice.net>
2012-06-28 15:00:31 +0200

    Move nested class defns to the bottom
    Since Clean Code's Stepdown Rule recommends higher level abstractions
    appear first, move nested class definitions to the bottom of the class.
    They're not the lowest level of abstraction present, but I'll be damned
    if I put them in between two method definitions.

--- hash_diff.rb
+++ hash_diff.rb
@@ -5,40 +5,6 @@
 
 class HashDiff
 
-  class Difference
-
-    attr_accessor :key, :value
-
-    def initialize(key, value)
-      @key, @value = key, value
-    end
-
-    def to_s
-      raise NotImplementedError
-    end
-
-    def ==(other)
-      self.class == other.class and key == other.key and value == other.value
-    end
-
-  end
-
-  class Removal < Difference
-
-    def to_s
-      "- #{@key.inspect}: #{@value.inspect}"
-    end
-
-  end
-
-  class Addition < Difference
-
-    def to_s
-      "+ #{@key.inspect}: #{@value.inspect}"
-    end
-
-  end
-
   def initialize(old, new)
     @old = old
     @new = new
@@ -129,6 +95,40 @@ class HashDiff
 
   def new_value
     @new[@key]
+  end
+
+  class Difference
+
+    attr_accessor :key, :value
+
+    def initialize(key, value)
+      @key, @value = key, value
+    end
+
+    def to_s
+      raise NotImplementedError
+    end
+
+    def ==(other)
+      self.class == other.class and key == other.key and value == other.value
+    end
+
+  end
+
+  class Removal < Difference
+
+    def to_s
+      "- #{@key.inspect}: #{@value.inspect}"
+    end
+
+  end
+
+  class Addition < Difference
+
+    def to_s
+      "+ #{@key.inspect}: #{@value.inspect}"
+    end
+
   end
 
 end
